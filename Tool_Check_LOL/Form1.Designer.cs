3e54dadd8cc6741f6289518cb4db82f8d6c22afb
mrpuaru <puarudz@gmail.com>
2019-11-12 01:31:16 +0700

    Upload

---
+++ Tool_Check_LOL/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Tool_Check_LOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kiểm Tra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(158, 102);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 120);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(158, 102);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(176, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(170, 210);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(28, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 78);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Đây là 1 sản phẩm của PuaruVN.\r\nGhé thăm website: AccRac.XYZ \r\nĐể ủng hộ tác giả " +
+    "nhé mọi người.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Chọn path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Tool_Check_LOL.Properties.Resources.league_of_legends_champion_clash_wallpaper404_com_1920x1200;
+            this.ClientSize = new System.Drawing.Size(360, 352);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tool Check Account LOL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Tool_Check_LOL/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Tool_Check_LOL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string LoLPath = "";
+        private string PathSave = @"C:\\PathLOL.txt";
+        private string[] lockfile;
+        private string authorizationToken;
+        private void BeGin()
+        {
+            Process[] processesByName = Process.GetProcessesByName("LeagueClient");
+            if (processesByName.Length != 0)
+            {
+                string[] source = processesByName[0].MainModule.FileName.Split(new char[]
+                {
+            '\\'
+                });
+                this.LoLPath = string.Join("\\", source.Take(source.Count<string>() - 1).ToArray<string>());
+            }
+            string path = this.LoLPath + "\\lockfile";
+            if (File.Exists(path))
+            {
+                Stream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                string text = new StreamReader(stream).ReadLine();
+                this.lockfile = text.Split(new char[]
+                {
+            ':'
+                });
+                string s = "riot:" + this.lockfile[3];
+                byte[] bytes = Encoding.UTF8.GetBytes(s);
+                this.authorizationToken = "Basic " + Convert.ToBase64String(bytes);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!File.Exists(PathSave))
+            {
+                MessageBox.Show("Chưa chọn Path LeagueClient kìa ba");
+                return;
+            }
+            StreamReader sr = new StreamReader(PathSave);
+            LoLPath = sr.ReadLine();
+            BeGin();
+            this.userData = JObject.Parse(this.lcuRequest("/lol-chat/v1/me", "GET", null));
+            JObject jobject = JObject.Parse(this.lcuRequest("/lol-summoner/v1/summoners/" + this.userData["id"].ToString(), "GET", null));
+            string Rich1 = "Name: " + this.userData["name"] + "\n";
+            Rich1 += "LV: " + jobject["summonerLevel"] + "\n";
+            Rich1 += "AFK: " + GetPenaltyTime() + "s\n";
+            GetChampionsAndSkins();
+            Rich1 += "Tướng: " + this.championsOwned.LongCount() + "\n";
+            Rich1 += "Skins: " + this.skinsOwned.LongCount() + "\n";
+            Dictionary<string, string> dictionary = this.GetRank();
+            try
+            {
+                Rich1 += "Rank DD: " + dictionary["RANKED_SOLO_5x5"] + "\n"; 
+            }
+            catch
+            {
+                Rich1 += "Rank DD: Chưa RANK\n";
+            }
+            try
+            {
+
+                Rich1 += "5VS5: " + dictionary["RANKED_FLEX_SR"] + "\n";
+            }
+            catch
+            {
+                Rich1 += "5VS5: Chưa RANK\n";
+            }
+            richTextBox1.Text = Rich1.ToString();
+            richTextBox2.Text = string.Join("|", this.championsOwned.ToArray());
+            richTextBox3.Text = string.Join("|", this.skinsOwned.ToArray());
+
+        }
+        private JObject userData;
+        private List<string> championsOwned = new List<string>();
+        private List<string> skinsOwned = new List<string>();
+        private string lcuRequest(string path, string type = "GET", string data = null)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = ((object senderX, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) => true);
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12);
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://127.0.0.1:" + this.lockfile[2] + path);
+            httpWebRequest.Method = type;
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:28.0) Gecko/20100101 Firefox/28.0";
+            httpWebRequest.Headers.Add("authorization", this.authorizationToken);
+            if (data != null)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(data);
+                }
+            }
+            string result;
+            using (StreamReader streamReader = new StreamReader(((HttpWebResponse)httpWebRequest.GetResponse()).GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            return result;
+        }
+        private string GetPenaltyTime()
+        {
+            JObject jobject = new JObject();
+            jobject.Add("queueId", 430);
+            try
+            {
+                this.lcuRequest("/lol-lobby/v2/lobby", "DELETE", null);
+            }
+            catch
+            {
+            }
+            this.lcuRequest("/lol-lobby/v2/lobby", "POST", jobject.ToString());
+            this.lcuRequest("/lol-lobby/v2/lobby/matchmaking/search", "POST", null);
+            string text = this.lcuRequest("/lol-lobby/v2/lobby/matchmaking/search-state", "GET", null);
+            if (text.Length == 0)
+            {
+                return "0.0";
+            }
+            string result;
+            try
+            {
+                JObject jobject2 = JObject.Parse(text);
+                result = jobject2["lowPriorityData"]["penaltyTime"].ToString();
+            }
+            catch
+            {
+                result = "0.0";
+            }
+            /*this.lcuRequest("/lol-lobby/v2/lobby/matchmaking/search", "DELETE", null);
+            this.lcuRequest("/lol-lobby/v2/lobby", "DELETE", null);*/
+            return result;
+        }
+        private void GetChampionsAndSkins()
+        {
+            this.championsOwned = new List<string>();
+            this.skinsOwned = new List<string>();
+            JArray jarray = JArray.Parse(this.lcuRequest("/lol-champions/v1/inventories/" + this.userData["id"].ToString() + "/champions", "GET", null));
+            foreach (JToken jtoken in jarray)
+            {
+                JObject jobject = (JObject)jtoken;
+                if (jobject["alias"].ToString() != "None" && jobject["ownership"]["owned"].ToString() == "True")
+                {
+                    this.championsOwned.Add(jobject["id"].ToString() + "-"+jobject["alias"].ToString());
+                    foreach (JToken jtoken2 in ((JArray)jobject["skins"]))
+                    {
+                        JObject jobject2 = (JObject)jtoken2;
+                        if (jobject2["isBase"].ToString() != "True" && jobject2["ownership"]["owned"].ToString() == "True")
+                        {
+                            this.skinsOwned.Add("championsskin_" + jobject2["id"].ToString() +"-"+jobject2["name"].ToString());
+                        }
+                    }
+                }
+            }
+            this.championsOwned = this.championsOwned.Distinct<string>().ToList<string>();
+            this.skinsOwned = this.skinsOwned.Distinct<string>().ToList<string>();
+        }
+        private Dictionary<string, string> GetRank()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            Dictionary<string, string> result;
+            try
+            {
+                JArray jarray = JArray.Parse(this.lcuRequest("/lol-ranked/v2/tiers?summonerIds=[" + this.userData["id"].ToString() + "]&queueTypes=[1,2]", "GET", null));
+                if (jarray.Count == 0)
+                {
+                    throw new Exception();
+                }
+                foreach (JToken jtoken in ((JArray)jarray.First<JToken>()["achievedTiers"]))
+                {
+                    JObject jobject = (JObject)jtoken;
+                    dictionary.Add(jobject["queueType"].ToString(), jobject["tier"].ToString() + " " + jobject["division"].ToString());
+                }
+                result = dictionary;
+            }
+            catch
+            {
+                result = new Dictionary<string, string>
+        {
+            {
+                "RANKED_SOLO_5x5",
+                "unrank"
+            },
+            {
+                "RANKED_FLEX_SR",
+                "unrank"
+            }
+        };
+            }
+            return result;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(PathSave, folderDlg.SelectedPath);
+                MessageBox.Show(folderDlg.SelectedPath);
+            }
+        }
+    }
+}
